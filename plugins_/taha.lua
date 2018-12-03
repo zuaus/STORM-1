d9fab2135f426fa9fd0a585e4804d325f034b0fe
zuaus <30482065+zuaus@users.noreply.github.com>
2018-12-03 08:45:09 +0300

    Update taha.lua

--- plugins_/taha.lua
+++ plugins_/taha.lua
@@ -1,1 +1,14 @@
-hi
+local function helpbot(msg)
+  if text == 'help' then
+
+taha = [[
+uuuuuu
+    خخخخخ
+]]
+
+storm_sendMsg(msg.chat_id_, msg.id_, 1,taha, 1, 'md') 
+
+end
+  end
+
+return {THESTORM=helpbot,}
